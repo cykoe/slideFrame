15694b48b2f5e55d8db9f79f5f09b8272b18b38b
cykoe <esculin.hao@gmail.com>
2020-06-29 14:33:47 -0500

    slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('modal') modal: any;
+  @ViewChild('modalContent') modalContent: any;
   title = 'slideIframe';
+
+  openBar() {
+    this.modal.nativeElement.style.display = 'block';
+    this.modalContent.nativeElement.style.transform = 'translateX(0)';
+  }
+
+  close() {
+    this.modal.nativeElement.style.display = 'none';
+    this.modalContent.nativeElement.style.transform = 'translateX(300px)';
+  }
 }
